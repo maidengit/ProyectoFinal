2e0264b8edb6de324e727e2b136750e9ac505e3c
Maiden <adonay22@outlook.com>
2020-08-09 10:46:45 -0600

    Agregue interfaz de Listado de Estudiantes

--- proyectoFinal/Properties/Resources.Designer.cs
+++ proyectoFinal/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace proyectoFinal.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Screenshot_244 {
+            get {
+                object obj = ResourceManager.GetObject("Screenshot_244", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
---
+++ proyectoFinal/listadoEstudiantes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoFinal
+{
+    public partial class listadoEstudiantes : Form
+    {
+        public listadoEstudiantes()
+        {
+            InitializeComponent();
+        }
+
+        private void filtro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
